e488dc7d6f62278efa9f2edb357bc0f501910af9
sir-kitter <sire.kitter@gmail.com>
2020-10-01 02:30:32 +0300

    init: start typical-arrays-problems

--- src/index.js
+++ src/index.js
@@ -1,12 +1,24 @@
-
-exports.min = function min (array) {
-  return 0;
+exports.min = function min(array) {
+    if (Array.isArray(array)) {
+        if (array.length === 0) return 0
+        return array.reduce((prevValue, currentValue) => prevValue < currentValue ? prevValue : currentValue, array[0])
+    }
+	return 0
 }
 
-exports.max = function max (array) {
-  return 0;
+exports.max = function max(array) {
+    if (Array.isArray(array)) {
+        if (array.length === 0) return 0
+        return array.reduce((prevValue, currentValue) => prevValue > currentValue ? prevValue : currentValue, array[0])
+    }
+    return 0
 }
 
-exports.avg = function avg (array) {
-  return 0;
+exports.avg = function avg(array) {
+    if (Array.isArray(array)) {
+        if (array.length === 0) return 0
+        const sum = array.reduce((accumulator, value) => accumulator + value)
+        return sum / array.length
+    }
+    return 0
 }
